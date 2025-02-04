a21878a0ad75db363baaec492e876b1b6f4adbe1
Apheayenne <zelda.ganondorf@gmail.com>
2025-02-03 19:36:53 -0500

    Inital Commit

---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LoadData : MonoBehaviour
+{
+    [SerializeField]
+    private string GameName;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    
+    }
+
+
+}
