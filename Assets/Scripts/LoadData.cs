543b3f14d8e61386a524aee15928f95ebff56fef
Apheayenne <zelda.ganondorf@gmail.com>
2025-02-12 17:42:12 -0600

    Repo Update

--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+using UnityEngine.InputSystem.UI;
 
 public class LoadData : MonoBehaviour
 {
-    [SerializeField]
-    private string GameName;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    
+//    [SerializeField]
+//    private string GameName;
+    public TextMeshProUGUI Textbox;
+    public UnityEvent onClick;
+
+    public void Start() { }
+
+
+
+	public void debug() {
+        Debug.Log("Pressed Button");
+	}
+
+	public void setText(string GameName){
+        Debug.Log("Setting text to: <" + GameName + ">");
+        Textbox.GetComponent<TextMeshProUGUI>().text = GameName;
     }
 
+	public void setAlpha() { }
 
+	
 }
+    
